d6c6523e79e34bf80a9e2c8a7ac9f372c2381e6e
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-08-26 13:46:10 +0600

    portfolio with django

---
+++ home/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Basic_info, Skill, Project
+
+admin.site.register(Basic_info)
+admin.site.register(Skill)
+admin.site.register(Project)
---
+++ home/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class SingletonModel(models.Model):
+    class Meta:
+        abstract = True
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+class Basic_info(SingletonModel):
+    name = models.CharField(max_length= 60)
+    image = models.ImageField(upload_to= 'image/')
+    about = models.TextField()
+    email = models.EmailField(max_length= 100)
+    linkedin = models.URLField(max_length= 50)
+    github = models.URLField(max_length= 50)
+    facebook = models.URLField(max_length= 50)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Skill(models.Model):
+    user = models.ForeignKey(Basic_info, on_delete=models.CASCADE)
+    technology_name = models.CharField(max_length= 100)
+    technology_icon = models.CharField(max_length= 100)
+    description = models.TextField(blank= True)
+
+    def __str__(self) -> str:
+        return self.technology_name
+    
+class Project(models.Model):
+    user = models.ForeignKey(Basic_info, on_delete= models.CASCADE)
+    title = models.TextField(max_length= 100)
+    image = models.ImageField(upload_to= 'image/project/')
+    description = models.TextField()
+    live_link = models.URLField(max_length= 100, blank= True)
+    github_repo_link = models.URLField(max_length= 100, blank= True)
+
+    def __str__(self) -> str:
+        return self.title
