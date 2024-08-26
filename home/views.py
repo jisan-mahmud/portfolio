a4ad4560d4d5749031e76779ecc64287f601811a
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-08-26 15:17:06 +0600

    show data on frontend from backend in hero section

--- home/views.py
+++ home/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Basic_info, Skill, Project
+from django.urls import reverse
 
-def home(request):
-    return render(request, 'home/index.html')
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        basic_info = Basic_info.objects.get(pk=1)
+        skills = Skill.objects.all()
+        projects = Project.objects.all()
+        context = {
+            'basic_info': basic_info,
+            'skills': skills,
+            'projects': projects
+        }
+        return render(request, 'home/index.html', context = context)
